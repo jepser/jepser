6c45af63909ea02b04cdb9061f47ac140c286e54
Jepser Bernardino <jepsersk8@gmail.com>
2017-04-29 20:36:37 +0200

    add v2 theme

---
+++ html/app/themes/jepser-v2/resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+<header class="banner">
+  <div class="banner__container">
+    <a class="banner__brand" href="{{ home_url('/') }}"><img src="@asset('images/jepser-logo.svg')" /></a>
+    <nav class="banner__navigation navigation">
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu([
+                'theme_location' => 'primary_navigation',
+                'menu_class' => 'nav',
+                'container'     => ''
+            ])
+        !!}
+      @endif
+    </nav>
+  </div>
+</header>
+@if (!is_front_page())
+<div class="page__title page-title">
+    <h1 class="page-title__content">
+        @if(is_single())
+            Blog
+        @elseif(is_category())
+            {{ single_cat_title() }}
+        @else
+            {{ the_title() }}
+        @endif
+    </h1>
+</div>
+@endif
---
+++ html/app/themes/jepser-v2/resources/views/template-home.blade.php
@@ -0,0 +1,11 @@
+{{--
+  Template Name: Homepage
+--}}
+
+@extends('layouts.app')
+@section('content')
+    <div class="home__container">
+        <h1 class="home__title">Soy Jepser Bernardino Front-End developer <br> & WordPress expert de Guatemala, viviendo en Barcelona.</h1>
+        <p class="home__title home__title--sub">— Trabajando en Typeform.</p>
+    </div>
+@endsection
