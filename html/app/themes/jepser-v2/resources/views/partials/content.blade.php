7d48547fd4262103d74c39029e00f9e076719e3f
Jepser Bernardino <jepsersk8@gmail.com>
2017-04-30 16:06:23 +0200

    add styles for desktop view

--- html/app/themes/jepser-v2/resources/views/partials/content.blade.php
+++ html/app/themes/jepser-v2/resources/views/partials/content.blade.php
@@ -1,7 +1,7 @@
 <article @php(post_class())>
   <header>
     @include('partials/entry-meta')
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
+    <h2 class="article__title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
   </header>
   <div class="entry-summary">
     @php(the_excerpt())
--- html/app/themes/jepser-v2/resources/views/partials/entry-meta.blade.php
+++ html/app/themes/jepser-v2/resources/views/partials/entry-meta.blade.php
@@ -1,9 +1,4 @@
 <div class="article__meta">
     <time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_modified_date() }}</time>
-    <p class="byline author vcard">
-        {{ __('By', 'sage') }}
-        <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
-            {{ get_the_author() }}
-        </a>
-    </p>
+    <div class="article__category">{{!! print_r(get_the_category()) !!}}</div>
 </div>
